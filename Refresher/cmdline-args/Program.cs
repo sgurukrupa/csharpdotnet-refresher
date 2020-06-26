5c287274e8486c635a98def0efd448082bc65891
Ganesh Hegde <sgurukrupa@gmail.com>
2020-06-26 13:28:03 +0530

    Added a project to print the arguments supplied at the command line to an executable, Haribol.

---
+++ Refresher/cmdline-args/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cmdline_args
+{
+    class Program
+    {
+        static void PrintArgs(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintArgs(args);
+        }
+    }
+}
