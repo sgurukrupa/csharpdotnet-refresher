ca39d111371b7d606d71d4e0fb9f3e3f7231878a
Ganesh Hegde <sgurukrupa@gmail.com>
2020-06-26 14:26:33 +0530

    Added explanation on the parameter of Main method.

--- Refresher/cmdline-args/Program.cs
+++ Refresher/cmdline-args/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 
+/*
+The Main method, which is a starting point for a C# application, can be declared with a string[] parameter.
+The parameter, which is an array, provides access to arguments supplied at the command-line when the executable is run.
+The first element of the array is the first argument to the executable and not the name / path of the executable as in C/C++.
+*/
+
 namespace cmdline_args
 {
     class Program
