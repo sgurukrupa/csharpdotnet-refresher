9e4f7c798ed46969fcbfd00f3620263e2659549b
Ganesh Hegde <sgurukrupa@gmail.com>
2020-06-27 20:45:53 +0530

    Added a basic hello-world project.

---
+++ Refresher/hello-world/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hello_world
+{
+    /*
+     * A basic C#.NET application consists of a class definition containing a single static method named Main
+     */
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
