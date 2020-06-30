1f76b93f0539329afbd2c718cef12b801f92ab19
Ganesh Hegde <sgurukrupa@gmail.com>
2020-06-30 14:55:28 +0530

    Added string interpolation, Haribol.

---
+++ Refresher/declaration-statements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace declaration_statements
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* Gyan 4
+             * A constant is declared by preceding the variable declaration with the keyword 'const'.
+             * A constant must always be initialized with a value.
+             */
+            const int i = 2;
+
+            /* Gyan 4
+             * A Variable is declared by following the type of the variable with an identifier.
+             * The variable can be optionally initialized with a value.
+             * Below, the variable 'j' is initialized, but the variable 'k' is not.
+             */
+            int j = i - 1, k;
+            Console.WriteLine(j);
+
+            /* Gyan 4.2
+             * C# forbids the use of an unitialized variable in an expression.
+             * Below, the variable 'k' is initialized with the value 0, before being used as argument for WriteLine method.
+             */
+            k = 0;
+            Console.WriteLine(k);
+        }
+    }
+}
---
+++ Refresher/string-interpolation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace string_interpolation
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            /* Gyan 5
+             * String interpolation means substituting C# expressions embedded inside a string literal with their values.
+             * A string literal is interpolated by preceding it with the '$' symbol; C# expressions enclosed by pairs of braces are replaced with their values.
+             * Below, the string argument is interpolated and the variable 'i' and the expression 'i + 1' are replaced with their respective values.
+             */
+            Console.WriteLine($"i: {i}, next i: {i + 1}");
+        }
+    }
+}
